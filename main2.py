c54a62b43fc46d5df77ec35fedf276fa1814ca0c
Paul <paul.mcbrien2@mail.dcu.ie>
2023-02-24 18:04:20 -0800

    replaced main and added env to gitignore

--- main2.py
+++
@@ -1,9 +0,0 @@
-from config.config import DevConfig
-import uvicorn
-from src.app import create_app
-
-app= create_app()
-
-if __name__ == "__main__":
-    print("Starting server...")
-    uvicorn.run("main:app", reload=True)
